196f738d4e25fb4c90d1fa3ddca06b47eb56ffef
Ryan-Jing <ryanjing23@gmail.com>
2025-06-05 22:15:10 -0400

    updating shell commands for building and emulating test program

--- .github/actions/unit-test-action/entrypoint.sh
+++ .github/actions/unit-test-action/entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 cd test
 idf.py build
 cd build
-esptool.py --chip esp32 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
-/opt/qemu/bin/qemu-system-xtensa -nographic -no-reboot -machine esp32 -drive file=flash_image.bin,if=mtd,format=raw -serial file:output.log
+esptool.py --chip esp32s3 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
+cd ..
+/opt/qemu/bin/qemu-system-xtensa -nographic -no-reboot -machine esp32s3 -drive file=build/flash_image.bin,if=mtd,format=raw -serial file:output.log
 results=$(grep -E '[[:digit:]]+ Tests [[:digit:]]+ Failures [[:digit:]]+ Ignored' output.log) || exit 1
+echo "========================== Unit Test Results =========================="
 echo "$results"
+echo "======================================================================="
 echo "$results" | grep -q '0 Failures' || exit 1
