0dcc77f9ada31022045fdc113e33248e1f82ab3c
Ryan-Jing <ryanjing23@gmail.com>
2025-06-06 01:55:45 -0700

    Command changes

--- .github/actions/unit-test-action/entrypoint.sh
+++ .github/actions/unit-test-action/entrypoint.sh
@@ -6,14 +6,15 @@ idf.py build
 cd build
 esptool.py --chip esp32s3 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
 cd ..
-/opt/qemu/bin/qemu-system-xtensa -nographic -no-reboot -machine esp32s3 -drive file=build/flash_image.bin,if=mtd,format=raw -serial file:output.log
-results=$(grep -E '[[:digit:]]+ Tests [[:digit:]]+ Failures [[:digit:]]+ Ignored' output.log) || exit 1
+# /opt/qemu/bin/
+qemu-system-xtensa -nographic -no-reboot -machine esp32s3 -drive file=build/flash_image.bin,if=mtd,format=raw -serial file:output.log
 
 # Print the full test log to the GitHub Actions output
 echo "========================== QEMU Serial Output =========================="
 cat output.log
 echo "======================================================================="
 
+results=$(grep -E '[[:digit:]]+ Tests [[:digit:]]+ Failures [[:digit:]]+ Ignored' output.log) || exit 1
 echo "========================== Unit Test Results =========================="
 echo "$results"
 echo "======================================================================="
