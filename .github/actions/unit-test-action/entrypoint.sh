7b8dbe44148c72614629d71564b277d2a0534e2a
Ryan-Jing <ryanjing23@gmail.com>
2025-06-06 02:06:09 -0700

    Updates for github action emulation, still not sucessful in executing the actual emulation

--- .github/actions/unit-test-action/entrypoint.sh
+++ .github/actions/unit-test-action/entrypoint.sh
@@ -6,15 +6,19 @@ idf.py build
 cd build
 esptool.py --chip esp32s3 merge_bin --fill-flash-size 4MB -o flash_image.bin @flash_args
 cd ..
-# /opt/qemu/bin/
-qemu-system-xtensa -nographic -no-reboot -machine esp32s3 -drive file=build/flash_image.bin,if=mtd,format=raw -serial file:output.log
+/opt/qemu/bin/qemu-system-xtensa -nographic -no-reboot -machine esp32s3 -drive file=build/flash_image.bin,if=mtd,format=raw -serial file:output.log
 
-# Print the full test log to the GitHub Actions output
 echo "========================== QEMU Serial Output =========================="
 cat output.log
 echo "======================================================================="
 
 results=$(grep -E '[[:digit:]]+ Tests [[:digit:]]+ Failures [[:digit:]]+ Ignored' output.log) || exit 1
+
+if command -v ruby >/dev/null && [ -f /opt/Unity-2.5.2/auto/parse_output.rb ]; then
+    ruby /opt/Unity-2.5.2/auto/parse_output.rb -xml output.log || true
+    [ -f report.xml ] && mv report.xml /github/workspace
+fi
+
 echo "========================== Unit Test Results =========================="
 echo "$results"
 echo "======================================================================="
